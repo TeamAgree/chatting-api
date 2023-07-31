297d197e1b94ba580d4de7c5e39f75fdd05674bb
haksung <hak9501@gmail.com>
2023-07-31 17:30:38 +0900

    Add CORS configuration
    CORS 설정 추가
    
    - addCorsMappings 메서드 오버라이드하여 CORS설정 추가
    
    관련 이슈: #
    참고 이슈: #

--- src/main/java/com/agree/chattingapi/conf/WebConfig.java
+++ src/main/java/com/agree/chattingapi/conf/WebConfig.java
@@ -1,8 +1,8 @@
 package com.agree.chattingapi.conf;
 
 import com.agree.chattingapi.utils.UserInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(userInterceptor);
     }
-}
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://34.64.251.46:8082")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
