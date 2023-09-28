b1d195a99e5125ac0039844ab45e4d2dba59a68c
haksung <hak9501@gmail.com>
2023-09-28 20:05:10 +0900

    Add SwaggerConfig
    Add SwaggerConfig
    
    - Add SwaggerConfig
    
    관련 이슈: #
    참고 이슈: #

---
+++ src/main/java/com/agree/chattingapi/conf/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.agree.chattingapi.conf;
+
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Chatting API")
+                        .description("agree team chatting api")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("agree team chatting api Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+}
