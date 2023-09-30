726a57dd7c2ed99dbae48598b2bcfb4a9fc8674a
haksung59 <hak9501@gmail.com>
2023-09-30 13:40:34 +0900

    test File Upload API file upload test
    -
    
    관련 이슈: #
    참고 이슈: #

--- src/main/java/com/agree/chattingapi/entities/FileInfoId.java
+++ src/main/java/com/agree/chattingapi/entities/FileInfoId.java
@@ -7,4 +7,11 @@ public class FileInfoId implements Serializable {
     private Long id;
     private String fileName;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 }
