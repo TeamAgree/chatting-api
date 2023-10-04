6a2bb3ca7de71c5313293245ec7baf1462294a53
haksung59 <hak9501@gmail.com>
2023-10-04 18:04:48 +0900

    test

--- src/main/java/com/agree/chattingapi/conf/WebConfig.java
+++ src/main/java/com/agree/chattingapi/conf/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(webIp, "http://code-server:3000")
+                .allowedOrigins(webIp, "http://code-server:3000", "http://code-server:23000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
