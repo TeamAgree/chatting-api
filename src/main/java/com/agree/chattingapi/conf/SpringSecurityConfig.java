d85e2dd2def1bce84eca0fed6ab421434a07d801
haksung59 <hak9501@gmail.com>
2023-10-04 16:45:31 +0900

    test

--- src/main/java/com/agree/chattingapi/conf/SpringSecurityConfig.java
+++ src/main/java/com/agree/chattingapi/conf/SpringSecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
